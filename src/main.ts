db72da76be185f4bc22a3fdac15c066c1054f87b
parsasi <mail@parsa.pro>
2022-04-12 18:18:33 -0700

    `class-validator` basic setup is configured

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { ValidationPipe } from '@nestjs/common'
+
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist : true
+  }));
+
+  const configService: ConfigService = app.get(ConfigService);
+
+  const port = configService.get('SERVER_PORT');
+
+  await app.listen(port);
 }
 bootstrap();
